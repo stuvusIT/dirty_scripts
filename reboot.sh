55e5232f8341191fc5061e9d7312a89600dcb022
Mr. Pi <git-contact@mr-pi.de>
2018-10-01 11:59:13 +0200

    Init repo and add reboot script

---
+++ reboot.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+BASE_DIR="$( dirname "$( realpath -s "${BASH_SOURCE[0]}" )" )/"
+cd $BASE_DIR
+
+function LOGIT {
+	echo -e "\n\e[1;32m$*\e[0m\n"
+}
+function ERROR {
+	echo -e "\e[1;31m$*\e[0m"
+}
+function HR {
+	echo ""
+	printf "\e[1;31m%*s\e[0m\n" "${COLUMNS:-$(tput cols)}" "" | tr " " -
+	printf "\e[1;31m%*s\e[0m\n" "${COLUMNS:-$(tput cols)}" "" | tr " " -
+	echo ""
+}
+
+source config.inc.sh
+
+HR
+LOGIT "Stop all vms on hypervisor01 - execute »safe_restart_vms.sh« on hypervisor01\nPress return to continue"; read
+scp safe_restart_vms.sh $REMOTE_USER@$HYPERVISOR_IP:/tmp/safe_restart_vms.sh
+ssh -t $REMOTE_USER@$HYPERVISOR_IP bash /tmp/safe_restart_vms.sh stop
+HR
+
+LOGIT "Reboot and wait for server to come back online (Press Ctrl+a followed by 'k' after you see the login screen)\nPress return to continue"; read
+ssh $REMOTE_USER@$HYPERVISOR_IP "sudo shutdown -r &"
+ssh -t $REMOTE_USER@129.69.139.1 sudo screen /dev/ttyUSB0 115200
+
+HR
+LOGIT "Re(start) all vms on hypervisor01 - execute »safe_restart_vms.sh« on hypervisor01 and reboot\nPress return to continue"; read
+scp safe_restart_vms.sh $REMOTE_USER@$HYPERVISOR_IP:/tmp/safe_restart_vms.sh
+ssh -t $REMOTE_USER@$HYPERVISOR_IP bash /tmp/safe_restart_vms.sh
+HR
+
+for sec in {0..60}; do
+	echo -n -e "\e[1;33m\rWait $sec/60 seconds for stuvus.de\e[0m\n"
+	curl -m 10 -i https://stuvus.uni-stuttgart.de 2>/dev/null | head -n 1 | grep -q '200' && break
+done
+
+LOGIT "Fix mtu on legacy hosts"
+for host in mail01 sympa ldap01 imap01; do
+	echo -e "\tFix mtu on $host"
+	ssh root-mmroch@$host sudo ip l set mtu 1400 eth0
+done
+
+LOGIT "Restart openvpn services"
+for host in openvpn01 openvpn02; do
+	echo -e "\tRestart and fix openvpn on $host"
+	ssh $REMOTE_USER@$host sudo systemctl restart openvpn@openvpn_udp_1194.service
+	ssh $REMOTE_USER@$host sudo brctl show | grep -q tap || ERROR "Failed on $host"
+done
+
+LOGIT "Fix imap01 (restart dovecot and exim4)"
+echo -e "\trestart dovecot"
+ssh root-mmroch@imap01 sudo systemctl restart dovecot 
+echo -e "\trestart exim4"
+ssh root-mmroch@imap01 sudo systemctl restart exim4
+
+LOGIT "Fix mail01 (restart exim4)"
+echo -e "\trestart exim4"
+ssh root-mmroch@mail01 sudo systemctl restart exim4
+
+LOGIT "Reset malicious failed state on legacy vms"
+for host in mail01 sympa ldap01 imap01; do
+	echo -e "\tReset failed state on $host"
+	ssh root-mmroch@$host sudo systemctl reset-failed systemd-modules-load.service
+done
+
+LOGIT "Check finanz vm"
+xfreerdp /u:$REMOTE_USER /d:samba.faveve.uni-stuttgart.de /v:129.69.139.57
+
+LOGIT "Check for failed services"
+ssh hypervisor grep ip /etc/xen/vms/\*.cfg | sed 's/.*ip\=\([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\).*/\1/g'
+
+LOGIT "Check for failed services"
+for ip in `ssh $REMOTE_USER@hypervisor01 grep 'ip=' /etc/xen/vms/\*.cfg | sed 's/.*ip\=\([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\).*/\1/g' | sort | uniq`; do
+	echo -ne "\tcheck ›$ip‹ "
+	if ! ping -c 1 -W 1 $ip >/dev/null; then
+		echo -e "\r\e[33m\tskip ip ›\e[0m$ip\e[33m‹ (can't ping)\e[0m"
+		continue
+	fi
+	use_user="$REMOTE_USER"
+	[[ " 129.69.139.50 129.69.139.70 129.69.139.72 129.69.139.71 " =~ .*\ $ip\ .* ]] && use_user="root-mmroch"
+	if nmap -p 22 $ip | grep -q filtered; then
+		echo -e "\r\e[33m\tskip ip ›\e[0m$ip\e[33m‹ (ssh is closed)\e[0m"
+		continue
+	fi
+	if ssh $use_user@$ip sudo systemctl --failed | grep -q "0 loaded units listed."; then
+		host_name=`ssh $use_user@$ip hostname`
+		echo -e "\r\e[32m\tno failed units on ›\e[0m$ip\e[32m‹\e[0m ($host_name)"
+		continue
+	fi
+	echo -e "\r\e[1;31m\tfailed units on ›\e[0m$ip\e[1;31m‹\e[0m"
+	ssh $use_user@$ip sudo systemctl --failed
+	ssh $use_user@$ip
+done
+
+echo -e "\e[1;31m\n\n\tPlease keep in mind to check Jira manually\n\n"
---
+++ safe_restart_vms.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+function LOGIT {
+	echo -e "\n\e[1;32m$*\e[0m\n"
+}
+
+LOGIT "show all running vms"
+sudo xl list
+
+LOGIT "shutdown all running xen_vman managed vms"
+cd /etc/xen/vms/
+for vm in *; do
+	vm="${vm//.cfg}"
+	sudo xl shutdown -F $vm 2>&1 | grep -E -v "(invalid domain identifier|Shutting down domain|sending ACPI power button event)" &
+done
+
+if [ $((`sudo xl list | wc -l`-2)) -gt 0 ]; then
+echo -e "\e[1;31mWait 3minutes for all vms to shutdown gracefully before destroy them via \`xl destroy\`\e[0m\n"
+for sec in {0..180}; do
+	rem_vms=$((`sudo xl list | wc -l`-2))
+	echo -n -e "\r\tWait $sec/180 seconds ($rem_vms vms are still running, press 'x' to destroy them immediately)"
+	read -n 1 -N 1 -t 1 key
+	if [ $rem_vms -le 0 -o "$key" = "x" ]; then
+		break
+	fi
+done
+fi
+
+if [ $((`sudo xl list | wc -l`-2)) -gt 0 ]; then
+sudo xl list
+LOGIT "destroy all all remaining $((`sudo xl list | wc -l`-2)) vms"
+for vm in *; do
+	vm="${vm//.cfg}"
+	sudo xl destroy $vm 2>&1 | grep -E -v "(invalid domain identifier|Shutting down domain)" &
+done
+wait
+fi
+
+LOGIT "reset failed state of all vms"
+for vm in *; do
+	vm="${vm//.cfg}"
+	sudo systemctl reset-failed vm@$vm 2>&1 | grep -v "Failed to reset failed state" &
+done
+wait
+
+LOGIT "show all failed units"
+sudo systemctl --failed
+
+cd /dev/disk/by-path/
+if [ $(ls ip* 2>/dev/null | wc -l) -gt 0 ]; then
+LOGIT "logout all iscsi devices via systemctl"
+for vm in *; do
+	vm="${vm//.cfg}"
+	sudo systemctl stop "vm_iscsi@$vm" &
+done
+wait; sleep 5
+fi
+
+if [ $(ls ip* 2>/dev/null | wc -l) -gt 0 ]; then
+LOGIT "logout from all remaining iscsi devices"
+for disk in `ls ip* 2>/dev/null | grep -v "part[0-9]$" | sed 's/.*\(iqn\..*\)-lun.*/\1/'`; do
+	sudo iscsiadm -m node -T $disk --logout -p 129.69.139.18:3260
+done
+fi
+
+if [ $(ls ip* 2>/dev/null | wc -l) -gt 0 ]; then
+	echo -e "\e[1;31mFailed to logout from following iscsi devices:\e[0m"
+	ls ip*
+	echo "Press return to continue"
+	read
+fi
+
+if [ "$1" = "stop" ]; then
+	exit 0
+fi
+
+LOGIT "login into all iscsi devices"
+cd /etc/xen/vms/.iscsi/
+for vm in *; do
+	sudo systemctl start "vm_iscsi@$vm"
+done
+
+LOGIT "manual login into finanzen iscsi devices"
+sudo iscsiadm -m node -T iqn.2003-01.org.linux-iscsi.storage01.x8664:stuvus-finanzen-data --login -p 129.69.139.18:3260
+sudo iscsiadm -m node -T iqn.2003-01.org.linux-iscsi.storage01.x8664:stuvus-finanzen-system --login -p 129.69.139.18:3260
+
+for sec in {0..20}; do
+	echo -n -e "\r\tWait $sec/20 seconds"
+	sleep 1
+done
+echo -n -e "\r                              \r"
+
+LOGIT "show all failed units"
+sudo systemctl --failed
+
+LOGIT "start all enabled vms"
+cd /etc/xen/vms/
+for vm in *; do
+	vm="${vm//.cfg}"
+	if grep -q -v "$vm" /etc/xen/disabled_vms.txt; then
+		echo -e "\t\e[32mstart vm ›\e[0m$vm\e[32m‹\e[0m"
+		sudo systemctl start vm@$vm
+		sleep 0.5
+	else
+		echo -e "\t\e[33mskip vm\e[0m$vm"
+	fi
+done
+
+LOGIT "show all failed units"
+sudo systemctl --failed
+
+LOGIT "show all running vms"
+sudo xl list
